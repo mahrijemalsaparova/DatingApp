b4ace08707d4786360ade2914caec33026e1aa6d
mahri saparova <mahrijemal.saparova@gmail.com>
2020-07-16 18:39:07 +0300

    End of Section 6

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -5,49 +5,44 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    //AuthRepository QUERING OUR DATABASE BY ENTITYFRAMEWORK
+
     public class AuthRepository : IAuthRepository
     {
-        ////////INJECTION OF  OUR DATABASE////////////
+
         private readonly DataContext _context;
-        public AuthRepository(DataContext context) //ctor 
+        public AuthRepository(DataContext context) 
         { 
             _context = context;
-
         }
-        /////////////LOGIN METHOD//////////////////
         public async Task<User> Login(string username, string password)
         {
             var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
            
             if(user == null)
                return null;
-//parametrenin içindeki userdan aaldığı password ve doğru veya yanlış dödüren metoddur.
+
             if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
               return null;
 
             return user;
         }
-
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-             using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) //key
+             using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             { 
                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-               for(int i = 0 ; i > computedHash.Length; i++)
-               {//is not equal to
-                    if (computedHash[i] != passwordHash[i]) return false; //if these are two match means password is correct
+               for(int i = 0 ; i < computedHash.Length; i++)
+               {
+                    if (computedHash[i] != passwordHash[i]) return false; 
                }
                return true;
             }
             
         }
-        ////REGISTER METHOD////
         public async Task<User> Register(User user, string password)
-        {//converting password into passwordHash and the passwordSalt
-            byte[] passwordHash, passwordSalt;      //declaring variables
-            CreatePasswordHash(password, out passwordHash,out passwordSalt);  //The out is a keyword in C# which is used for the passing the arguments to methods as a reference type. It is generally used when a method returns multiple values.
-
+        {
+            byte[] passwordHash, passwordSalt;   
+            CreatePasswordHash(password, out passwordHash,out passwordSalt);  
 
              user.PasswordHash = passwordHash;
              user.PasswordSalt = passwordSalt;
@@ -66,7 +61,7 @@ namespace DatingApp.API.Data
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
- //////////////////LOGIC FOR THE USEREXIST////////////////////////
+
         public async Task<bool> UserExists(string username)
         {
            if(await _context.Users.AnyAsync(x => x.Username == username))
@@ -74,6 +69,6 @@ namespace DatingApp.API.Data
             
           return false;
         }
-////////////////////////////////////////////////////////////////
+
     }
 }
