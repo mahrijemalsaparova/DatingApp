8cab19d3916e8474f323ed7df18ea952f2ffdb38
mahri saparova <mahrijemal.saparova@gmail.com>
2020-07-19 12:59:10 +0300

    End Of Section 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -4,20 +4,12 @@ using Microsoft.EntityFrameworkCore;
 namespace DatingApp.API.Data
 {
     public class DataContext : DbContext //EntityFramwork'ün Dbcontext clası.
-    {   
-        /*hangi veritabanına bağlanıcak
-        DbContextOptions generic sınıflı bir configirasyondur. Buraya DataContext'mizi options olarak geçiyoruz.
-        options'u da base' e gönderiyoruz.
-        base DbContext'tır.
-        options: hangi veritabanına gidilecek?
-        */
-        public DataContext(DbContextOptions<DataContext> options) : base(options){} //ctor contranctor oluşturma
-           
-           
-           
-        public DbSet<Value> Values { get; set; } //DbSet Property --->Modeldeki Value ile Vertabanındaki 
-                                                  //Values tablosu ile ilişkilendir demek.
-         public DbSet<User> Users { get; set; }
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-        }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+
+    }
 }
